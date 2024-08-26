714390be825335715320fc19894f554fd3104fb1
Wuttha <titadach15632441@gmail.com>
2024-08-26 15:30:11 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import BMICalculatorComponent from "./BMICalculatorComponent";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <BMICalculatorComponent />
+    </>
+  );
+}
+
+export default App;
